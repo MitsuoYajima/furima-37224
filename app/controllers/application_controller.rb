9158668f0a3d7ff33932cf8c71db38b45799ae05
MitsuoYajima <mit.yaji0904@gmail.com>
2022-04-12 19:46:42 +0900

    新規登録、ログアウト、ログイン機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :basic_auth
+  # before_action :basic_auth
 
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name])
+    # binding.pry
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday])
   end
 
   def basic_auth
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nick_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birthday, presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数字を両方含む必要があります' }
+
 end
