b3ab545c0c66fbadaca2c29b5fb71879d70dad0e
MitsuoYajima <mit.yaji0904@gmail.com>
2022-04-15 02:37:21 +0900

    ActiveHashを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,9 @@ class Item < ApplicationRecord
   has_one :order
   belongs_to :user
   has_one_attached :image
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :days_to_ship
 end
