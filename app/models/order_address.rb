8f3a18c1429020f3642463caf00ed2cc6031ca2c
MitsuoYajima <mit.yaji0904@gmail.com>
2022-05-08 10:14:39 +0900

    商品購入機能　エラーハンドリング修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,18 +3,18 @@ class OrderAddress
   attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :token
 
   with_options presence: true do
+    validates :token
     validates :user_id
     validates :item_id
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input Correctly"}
     validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank" } 
     validates :municipality
     validates :address
-    # validates :phone_number, format: {with: /\d{10,11}\z/ , message: "Enter in 10 to 11 digits" }
-    validates :phone_number, length:{ minimum: 10,message:'is too short'}
+  end
+
+    validates :phone_number, presence: true, length:{ minimum: 10,message:'is too short'}
     validates :phone_number, length:{ maximum: 11,message:'is too long'}
     validates :phone_number, format: {with: /\A[0-9]+\z/, message: "Input only number" }
-    validates :token
-  end
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id,)
